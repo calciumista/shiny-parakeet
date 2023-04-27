ac4ae7cbaae15ab07d19458a6821954a73023aaa
calcalayy <minelandsmc14@gmail.com>
2023-04-27 14:56:25 +0100

    if statements :gasp:

--- stick/src/routes/+page.svelte
+++ stick/src/routes/+page.svelte
@@ -1,10 +1,16 @@
 <script>
+  import { is_empty } from "svelte/internal";
+
   let name = "calcium";
-  let describe = "gay";
+  let describe = "";
 </script>
 
 <div class="flex flex-col items-center px-4 py-4">
-  <h1 class="text-3xl font-bold text-white py-4">{name} is {describe}!</h1>
+  {#if describe}
+    <h1 class="text-3xl font-bold text-white py-4">{name} is {describe}!</h1>
+  {:else}
+    <h1 class="text-3xl font-bold text-white py-4">{name} is...</h1>
+  {/if}
   <input bind:value={name} placeholder="enter name!" class="bg-blue-300 text-center rounded-md" />
   <div class="py-2" />
   <input bind:value={describe} placeholder="enter adjective!" class="bg-blue-300 text-center rounded-md" />
