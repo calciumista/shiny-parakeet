26c79c1f4dbf796a368848af1d6b583bdc9216a7
calcalayy <minelandsmc14@gmail.com>
2023-04-28 17:28:24 +0100

    fixes yes

---
+++ stick/src/routes/info.svelte
@@ -0,0 +1,7 @@
+<div class="flex flex-col items-center px-4 py-4 text-white">
+  <h1 class="text-3xl font-bold text-white hover:scale-110 transition-all duration-200">Hi!</h1>
+  <div class="hover:scale-105 transition-all duration-200 text-bold">
+    <p>I'm Calcium, or Cal. I am currently a trial support at <a href="https://sirius.menu">Sirius</a>.</p>
+    <p>You can find me on <a href="https://discord.com">Discord</a> with the tag: 1018551539482230936</p>
+  </div>
+</div>
