3d05374f8c0b520f15caec8bad4608bf523b7da5
calcalayy <minelandsmc14@gmail.com>
2023-04-28 18:40:27 +0100

    improved certain things and fixed but with css. also improved the info section

--- stick/src/routes/name.svelte
+++ stick/src/routes/name.svelte
@@ -1,18 +1,18 @@
 <script>
   import "../app.css";
   let name = "calcium";
-  let describe = "daddy";
+  let describe = "the best svelte coder ever";
 </script>
 
 <div class="flex flex-col items-center px-4 py-4">
   {#if !name && !describe}
-    <h1 class="text-3xl font-bold text-white py-4 hover:scale-110 transition-all duration-200">something is...</h1>
+    <h1 class="text-3xl font-bold text-white hover:scale-110 transition-all duration-200">something is...</h1>
   {:else if !describe}
-    <h1 class="text-3xl font-bold text-white py-4 hover:scale-110 transition-all duration-200">{name} is...</h1>
+    <h1 class="text-3xl font-bold text-white hover:scale-110 transition-all duration-200">{name} is...</h1>
   {:else if !name && describe}
-    <h1 class="text-3xl font-bold text-white py-4 hover:scale-110 transition-all duration-200">something is {describe}!</h1>
+    <h1 class="text-3xl font-bold text-white hover:scale-110 transition-all duration-200">something is {describe}!</h1>
   {:else}
-    <h1 class="text-3xl font-bold text-white py- hover:scale-110 transition-all duration-2004">{name} is {describe}!</h1>
+    <h1 class="text-3xl font-bold text-white hover:scale-110 transition-all duration-2004">{name} is {describe}!</h1>
   {/if}
   <div class="py-2" />
   <input bind:value={name} placeholder="enter name!" class="bg-blue-300 text-center rounded-md shadow-md border-2 border-blue-200 hover:scale-105 transition-all duration-200 text-white text-bold" />
