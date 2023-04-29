875a65d5821ccf3ab1a6e30653cf539662944d6e
calcalayy <minelandsmc14@gmail.com>
2023-04-29 16:06:24 +0100

    fly-in + fixed bug with scale

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,25 @@
 <script>
+  import { onMount } from "svelte";
+  import { fly } from "svelte/transition";
   import Name from "./name.svelte";
   import Info from "./info.svelte";
   import Blox from "./bloxlink.svelte";
+
+  let ready = false;
+  onMount(() => (ready = true));
 </script>
 
 <svelte:head>
   <title>calcal.space</title>
 </svelte:head>
 
-<Info />
-<Name />
-<Blox />
+{#if ready}
+  <div transition:fly={{ y: 600, duration: 1000 }}>
+    <Info />
+    <Name />
+    <Blox />
+  </div>
+{/if}
 
 <style lang="postcss">
   :global(html) {
--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col items-center px-4 py-4 text-white hover:scale-105 transition-all duration-200">
+<div class="flex flex-col items-center px-4 py-4 text-white">
   <h1 class="text-3xl font-bold text-white text-center hover:scale-110 scale-110 transition-all duration-200 py-2">Hi!</h1>
   <div class="text-bold text-center">
     <p class="hover:scale-[1.065] transition-all duration-200 py-1">I'm Calcium, or Cal. I'm gay and I am currently a trial support at <a href="https://sirius.menu" class="font-outline-black">Sirius</a>.</p>
