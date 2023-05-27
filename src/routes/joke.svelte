83552ef943c47940d3001a7fb6ad0d17ef24c87f
calcalayy <minelandsmc14@gmail.com>
2023-05-27 08:06:19 +0100

    fix padding xoox

--- src/routes/joke.svelte
+++ src/routes/joke.svelte
@@ -24,6 +24,7 @@
 
 <div class="flex flex-col items-center px-2">
   <h1 class="text-3xl font-bold text-white text-center hover:scale-110 transition-all duration-200 py-2">Grab a Joke!</h1>
+  <div class="py-2" />
   <button on:click={grabJoke} class="blue-border text-bold px-10" id="lookup">Grab Joke!</button>
   <div class="py-2" />
   <div class="border-spacing-4">
