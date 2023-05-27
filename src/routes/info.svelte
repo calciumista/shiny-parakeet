b0ee49bc58716c1c43cd42157a6bb1039696d7a7
calcalayy <minelandsmc14@gmail.com>
2023-05-27 07:44:18 +0100

    sijsij

--- src/routes/info.svelte
+++ src/routes/info.svelte
@@ -3,5 +3,12 @@
   <div class="text-bold text-center">
     <p class="hover:scale-[1.065] transition-all duration-200 py-1">I'm Calcium, or Cal. I'm gay and I am currently a support member at <a href="https://sirius.menu" class="font-outline-black">Sirius</a>.</p>
     <p class="hover:scale-[1.065] transition-all duration-200 py-1">You can find me on <a href="https://discord.com" class="font-outline-blue">Discord</a> with the user id: 1018551539482230936.</p>
+    <!--
+      <div>
+      <div class="blue-border text-left px-2">
+        <h1 class="text-2xl font-bold">Sirius</h1>
+      </div>
+    </div>
+-->
   </div>
 </div>
