69a1cd09bd7f727f64ba4a1e760f1c497a75e46b
calcalayy <minelandsmc14@gmail.com>
2023-06-07 20:07:33 +0100

    made it more to site color palette

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,11 +7,12 @@
 </svelte:head>
 
 <div>
-  <div class="hero min-h-screen bg-blue-300">
-    <div class="hero-content text-center flex-col">
-      <h1 class="text-4xl text-neutral">calcal.space</h1>
+  <div class="hero min-h-screen bg-blue-400">
+    <div class="hero-content text-center flex-col text-white font-bold">
+      <h1 class="text-4xl text-white font-bold">calcal.space</h1>
       <h2 class="py-2 text-xl">Your one stop shop for Calcium! 🥛</h2>
       <a class="btn btn-secondary" href="{base}/main">Enter</a>
     </div>
   </div>
 </div>
+<!--wow-->
