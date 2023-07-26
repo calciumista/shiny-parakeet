fb233ec8e6c2e8bcc38eabc4131a2d2313836b7d
calcalayy <minelandsmc14@gmail.com>
2023-07-26 08:51:31 +0300

    jeh

--- routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<div class="hero justify-center min-h-screen">
-  <div class="hero-content text-center">
-    <div class="max-w-md">
-      <h1 class="text-primary text-xl font-bold pt-2">calcal.space</h1>
-      <p class="p-2 text-secondary">the best place for... calcium! :3</p>
-      <a href="/main" class="btn btn-primary">Get Started</a>
-    </div>
-  </div>
-</div>
