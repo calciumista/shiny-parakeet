6cd6173f799f13658c07ddaf3c089d37340830a9
calcalayy <minelandsmc14@gmail.com>
2023-07-26 08:38:01 +0300

    :3

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,11 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-import * as dotenv from 'dotenv';
-dotenv.config();
-
 declare global {
 	namespace App {
-		interface ProcessEnv {
-			BLOX: string;
-		}
+		// interface Error {}
+		// interface Locals {}
+		// interface PageData {}
+		// interface Platform {}
 	}
 }
 
