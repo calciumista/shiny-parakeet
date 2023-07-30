26344627f1a5b87a912fc74f000bf897c0a89312
calcalayy <minelandsmc14@gmail.com>
2023-07-30 14:16:42 +0300

    meow :P

---
+++ src/routes/main/components/work.svelte
@@ -0,0 +1,16 @@
+<div class="flex justify-center">
+    <div class="container grid gap-2 place-items-center">
+        <a href="https://switchhub.vercel.app">
+            <div class="tile-marker bg-blue-600 p-4 rounded hover:scale-105 transition-all duration-300">
+                <h1 class="font-bold text-lg pb-1">SwitchHub</h1>
+                <p>SwitchHub is a simple proof of concept app I made for Nintendo Homebrew. <br> It is one of my earlier projects. <br> It also has dynamic loading.</p>
+            </div>
+        </a>
+        <a>
+            <div class="tile-marker bg-blue-600 p-4 rounded hover:scale-105 transition-all duration-300">
+                <h1 class="font-bold text-lg pb-1">PocketChat</h1>
+                <p>PocketChat is probably my only backend experience. <br> It was one public chat made with SvelteKit and Pocketbase. <br> (pocketbase FTW).</p>
+            </div>
+        </a>
+    </div>
+</div>
