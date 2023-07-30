49e00d52a93fa55dcfca5013ca69fb5318582147
calcalayy <minelandsmc14@gmail.com>
2023-07-30 14:20:15 +0300

    rawr

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,7 @@
     <div class="max-w-md">
       <h1 class="text-primary text-xl font-bold pt-2">calcal.space</h1>
       <p class="p-2 text-secondary">the best place for... calcium! :3</p>
-      <a href="{base}/main" class="btn btn-primary">enter 😱</a>
+      <a href="/main" class="btn btn-primary">enter 😱</a>
     </div>
   </div>
 </div>
-
-<script lang="ts">
-  import { base } from '$app/paths';
-</script>
