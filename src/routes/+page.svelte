305e439c3a205d8ef699086815a93f7e17001396
calcalayy <minelandsmc14@gmail.com>
2023-07-30 14:31:26 +0300

    heehee

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,9 +9,7 @@
 <div>
   <div class="hero min-h-screen bg-blue-400">
     <div class="hero-content text-center flex-col text-white font-bold">
-      <h1 class="text-4xl text-white font-bold">calcal.space</h1>
-      <h2 class="py-2 text-xl">Your one stop shop for Calcium! 🥛</h2>
-      <a class="btn btn-secondary" href="{base}/main">Enter</a>
+      <h1 class="text-4xl text-white font-bold">calcal.space v2 soon</h1>
     </div>
   </div>
 </div>
